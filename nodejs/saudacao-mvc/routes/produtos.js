656998c5d5843b49b92cb6455ced91ba3b8d9c09
Henrique <henriquerfischborn@gmail.com>
2025-10-17 11:01:35 -0300

    Versão com rotas produtos

---
+++ nodejs/saudacao-mvc/routes/produtos.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const siteController = require('../controllers/siteController');
+
+
+
+router.get('/', siteController.produtos)
+
+
+router.get('/camisetas', siteController.camisetas)
+
+
+
+module.exports = router;
